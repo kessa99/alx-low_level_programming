7364ce152a591a8e9d1ac1ac32bfb69e162ca49b
kessa99 <kipre325@mail.com>
2022-09-09 14:27:03 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * man - prints exactl "and that piece of art is useful"
- * - Dora Korpar, 2015-10-19"
+ * man - prints exactl "and that piece of art is useful" - Dora Korpar, 2015-10-19"
  *
  * followed by a new line, to the standard output
  *
