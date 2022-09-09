d66d1d87210686cfba1e11c12d19cd39531512e4
kessa99 <kipre325@mail.com>
2022-09-09 14:10:06 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * man - print exactly the sentence "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * man - print exactly the sentence
+ *
  * followed by a new line, to the standard output
+ *
  * Return: 1 (sucess)
 */
 int main(void)
