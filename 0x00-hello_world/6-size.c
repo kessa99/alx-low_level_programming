5e284641a1608ecd6942ace4ec0c6233e2234f27
kessa99 <kipre325@mail.com>
2022-09-09 09:36:16 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,11 @@ int main(void)
 	float floatType;
 	double doubleType;
 	char charType;
+	long longType;
+	long long long_long;
 
+	printf("the size of long long is %zu bytes \n", sizeof(long_long));
+	printf("the size of long is %zu bytes\n", sizeof(longType));
 	printf("the size of int is %zu bytes\n", sizeof(intType));
 	printf("the size of char is %zu bytes\n", sizeof(charType));
 	printf("the size of float is %zu bytes\n", sizeof(floatType));
