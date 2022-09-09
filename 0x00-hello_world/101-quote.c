141f553ea372bc35f30448a409e54a3402d8846a
kessa99 <kipre325@mail.com>
2022-09-09 13:26:40 -0700

    100-intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * man - print the standard error
+ *
+ * Return: 1 (sucess)
+*/
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
