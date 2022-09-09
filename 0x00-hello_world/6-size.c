cbc996cb5cbf10b18bca3b18d760ad71d4255b21
kessa99 <kipre325@mail.com>
2022-09-09 10:37:22 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,30 +10,16 @@ int intType;
 float floatType;
 double doubleType;
 char charType;
-long longType;
-long long long_long;
-long double long_double;
-
-unsigned long long uns_long;
-unsigned short uns_short;
-unsigned int uns_int;
-unsigned char uns_char;
+long lg;
 long int lg_int;
 long long int lg_lg_int;
 
-printf("the size of long int is %zu bytes \n", sizeof(lg_int));
-printf("the size of long long int %zu bytes \n", sizeof(lg_lg_int));
-printf("the size of unsigned long long is %zu bytes\n", sizeof(uns_long));
-printf("the size of long double is %zu bytes \n", sizeof(long_double));
-printf("the size of unsigned long long is %zu bytes\n", sizeof(uns_short));
-printf("the size of unsigned short is %zu bytes \n", sizeof(uns_short));
-printf("the size of unsigned int is %zu bytes \n", sizeof(uns_int));
-printf("the size of unsigned char is %zu bytes \n", sizeof(uns_char));
-printf("the size of long long is %zu bytes \n", sizeof(long_long));
-printf("the size of long is %zu bytes\n", sizeof(longType));
-printf("the size of int is %zu bytes\n", sizeof(intType));
-printf("the size of char is %zu bytes\n", sizeof(charType));
-printf("the size of float is %zu bytes\n", sizeof(floatType));
-printf("the size of double is %zu bytes\n", sizeof(doubleType));
+printf("the size of long int is %zu byte(s) \n", sizeof(lg_int));o
+printf("the size of long long int %zu byte(s) \n", sizeof(lg_lg_int));
+printf("the size of long is %zu byte(s)\n", sizeof(lg));
+printf("the size of int is %zu byte(s)\n", sizeof(intType));
+printf("the size of char is %zu byte(s)\n", sizeof(charType));
+printf("the size of float is %zu byte(s)\n", sizeof(floatType));
+printf("the size of double is %zu byte(s)\n", sizeof(doubleType));
 return (0);
 }
