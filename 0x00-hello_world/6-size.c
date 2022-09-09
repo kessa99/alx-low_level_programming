e75c4882a0c625245438f0a4dc04a9472eadafee
kessa99 <kipre325@mail.com>
2022-09-09 11:01:11 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ printf("size of a long int: %zu byte(s)\n", sizeof(lg_int));
 printf("size of a long long int: %zu byt(s)\n", sizeof(lg_lg_int));
 printf("size of an int: %zu byt(s)\n", sizeof(intType));
 printf("size of a char: %zu byt(s)\n", sizeof(charType));
-printf("the size of float: %zu byt(s)\n", sizeof(floatType))
+printf("the size of float: %zu byt(s)\n", sizeof(floatType));
 return (0);
 }
