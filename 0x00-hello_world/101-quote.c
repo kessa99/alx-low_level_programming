cbf5c517e5b32b0c0da54b7479e2e2cdf132773d
kessa99 <kipre325@mail.com>
2022-09-09 13:59:38 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * man - print the standard error
- *
+ * man - print exactly the sentence "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ *followed by a new line, to the standard output
  * Return: 1 (sucess)
 */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+return (1);
 }
