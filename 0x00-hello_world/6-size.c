2194aea300e4879eb65a944269432b0aafb75e2b
kessa99 <kipre325@mail.com>
2022-09-09 10:41:52 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,8 @@ long lg;
 long int lg_int;
 long long int lg_lg_int;
 
-printf("the size of long int is %zu byte(s) \n", sizeof(lg_int));o
-printf("the size of long long int %zu byte(s) \n", sizeof(lg_lg_int));
+printf("the size of long int is %zu byte(s)\n", sizeof(lg_int));
+printf("the size of long long int %zu byte(s)\n", sizeof(lg_lg_int));
 printf("the size of long is %zu byte(s)\n", sizeof(lg));
 printf("the size of int is %zu byte(s)\n", sizeof(intType));
 printf("the size of char is %zu byte(s)\n", sizeof(charType));
