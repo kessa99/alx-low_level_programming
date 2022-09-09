27f14b145c2ed21eb9f0fb5b8e137e9f08e70023
kessa99 <kipre325@mail.com>
2022-09-09 09:56:51 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,18 @@ int main(void)
 	char charType;
 	long longType;
 	long long long_long;
+	long double long_double;
+	unsigned long long uns_long;
+	unsigned short uns_short;
+	unsigned int uns_int;
+	unsigned char uns_char;
 
+	printf("the size of unsigned long long is %zu bytes\n", sizeof(uns_long));
+	printf("the size of long double is %zu bytes \n", sizeof(long_double));
+	printf("the size of unsigned long long is %zu bytes\n", sizeof(uns_short));
+	printf("the size of unsigned short is %zu bytes \n", sizeof(uns_short));
+	printf("the size of unsigned int is %zu bytes \n", sizeof(uns_int));
+	printf("the size of unsigned char is %zu bytes \n", sizeof(uns_char));
 	printf("the size of long long is %zu bytes \n", sizeof(long_long));
 	printf("the size of long is %zu bytes\n", sizeof(longType));
 	printf("the size of int is %zu bytes\n", sizeof(intType));
