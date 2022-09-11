dad368670cbff3c6da6f3980d4c91b739258bf7f
kessa99 <kipre325@mail.com>
2022-09-10 19:33:42 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * main - print alphabet
+ *
+ * Return: 0 (sucess)
+*/
+
+int main(void)
+{
+	char c;
+	for(c = 'a'; c <= 'z'; c++)
+	       putchar(c);
+	return (0);	
+}
