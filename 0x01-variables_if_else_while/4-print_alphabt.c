88763ae7bb9fb128f96dcd46e4435b44a308089b
kessa99 <kipre325@mail.com>
2022-09-11 18:18:38 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet without q and e
+ *
+ * Return: 0 (sucess)
+*/
+
+int main(void)
+{
+	char c;
+	for (c = 'a' ; c <= 'z' ; c++)
+		if (c == 'q' && c == 'e')
+			putchar(c);
+	putchar('\n');
+	return (0);
+}
