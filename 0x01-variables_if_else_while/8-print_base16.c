d917dd05161b7b155bafeeefa164d7b22dd7e957
kessa99 <kipre325@mail.com>
2022-09-11 19:52:35 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print digit in base 16 with putchar
+ *
+ * Return: 0(sucess)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 16 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
