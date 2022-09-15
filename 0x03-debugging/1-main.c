d0283c2253a74eab334da2ee349ad260344b9807
kessa99 <kipre325@mail.com>
2022-09-15 05:56:33 -0700

    comment

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,1 +1,22 @@
-We have a infinite loop
+#include "main.h"
+
+/**
+ * main - causes an infinite loop
+ *
+ * Retrun: 0
+*/
+
+int main(void)
+{
+	int i;
+	printf("Infinite loop incoming :(\n");
+	i = 0;
+	/**
+	 * while (i < 10)
+	 * {
+	 * 	putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
