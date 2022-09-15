1a0c80f1ee9d3619adb5c1373547060a6f949e0e
kessa99 <kipre325@mail.com>
2022-09-15 06:13:40 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -15,11 +15,10 @@ int main(void)
 	/**
 	 * this is the infinite loop
 	 */
-	  while (i < 10)
-	  {
-	  putchar(i);
-	  }
-	 
+	 while (i < 10)
+	 {
+		 putchar(i);
+	 }
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
