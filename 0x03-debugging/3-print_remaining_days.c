4fe1758d8c41b33f89fe0005d0048d96f4a74296
kessa99 <kipre325@mail.com>
2022-09-16 06:45:06 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || (year % 400 == 0 && !(year % 100 == 0)))
+	if ((year % 4 == 0 && year % 100 !== 0) || (year % 400 == 0))
 	{
 		if (month >= 2)
 		{
@@ -20,6 +20,7 @@ void print_remaining_days(int month, int day, int year)
 		}
 		printf("Day of year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
+	}
 	else
 	{
 		if (month == 2 && day == 60)
@@ -28,8 +29,9 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-				printf("Day of the year: %d\n", day);
-				printf("Remaining days: %d\n", 365 - day);
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
+	return (0);
 }
