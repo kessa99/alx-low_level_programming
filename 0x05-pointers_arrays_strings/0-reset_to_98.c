0744c8c7c1f0d107a00f2329724072590ef70205
kessa99 <kipre325@mail.com>
2022-09-20 08:31:48 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - take a pointer to an int as parameter
+ *@n: is a pointer
+ * Return: 0 (sucess)
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void reset_to98(int *n)
+#endif
