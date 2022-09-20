5b6640a9deedf41cd3c695e3e40bc7338f0be3e3
kessa99 <kipre325@mail.com>
2022-09-19 19:21:29 -0700

    use fonction _putchar

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check my code
+ *
+ * Return:0 (sucess)
+*/
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/voiture.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print hello
+ *
+ * Return: 0 (sucess)
+*/
+
+int main(void)
+{
+	printf  ("Hello!\n");
+	return (0 );
+}
