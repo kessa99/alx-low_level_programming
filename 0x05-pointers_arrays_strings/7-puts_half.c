bb4d6eeb16c278932aeddc004bdd74a132f8df9a
kessa99 <kipre325@mail.com>
2022-09-20 21:00:17 -0700

    winnig is only half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - program half of string
+ * @str: parameter
+ * Return: void
+*/
+
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+
+	str = str - (len / 2);
+	
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
