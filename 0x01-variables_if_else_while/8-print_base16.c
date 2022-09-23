620a0b4a71900f01f36def6ff8fe7da605494cd2
kessa99 <kipre325@mail.com>
2022-09-23 00:06:21 -0700

    16bytes

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,9 +9,16 @@
 int main(void)
 {
 	int i;
+	char c[6] = "abcdef";
 
-	for (i = 0 ; i < 16 ; i++)
-		putchar((i % 10) + '0');
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i+ '0');
+	}
+	for (i = 0 ; i < 6 ; i++)
+	{
+		putchar(c[i]);
+	}
 	putchar('\n');
 	return (0);
 }
