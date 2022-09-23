c7c4c870662663c1457300ec557109acb76c0dca
kessa99 <kipre325@mail.com>
2022-09-22 23:26:00 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * man - prints exactl "and that piece of art is useful" - Dorls
- *eKorpar, 2015-10-19"
- *
- * followed by a new line, to the standard output
+ * main - print exactly a text
  *
  * Return: 1 (sucess)
 */
+
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
 }
