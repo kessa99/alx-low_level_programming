dd5071f6ed0e58859854654df02ca8928c054495
kessa99 <kipre325@mail.com>
2022-09-23 21:34:19 -0700

    tasks more pointer

---
+++ 0x06-pointers_array_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - function strcat
+ * @dest:parameter
+ * @src: second parameter
+ * Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len = 0;
+	int i;
+
+	while (dest[len])
+		len++;
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[len] = src[i];
+		len += 1;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_array_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncat - concatenate two strings
+ * @dest: dest value
+ * @src: source value
+ * @n:parameter to compare index 
+ * Return: returns new concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
