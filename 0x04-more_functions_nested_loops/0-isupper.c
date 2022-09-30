4c9ef6e0d01bd9340d5cae00e64fc3bf278e7101
kessa99 <kipre325@mail.com>
2022-09-30 11:35:08 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isupper - function for uppercase character
+ * @c: integer
+ * Return:0 (sucess)
+*/
+
+int _isupper(int c)
+{
+	int i;
+
+	for (i = 'A' ; i <= 'Z' ; i++)
+	{
+		if (c == i)
+		{
+			return (0);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+return (0);
+}
