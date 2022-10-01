7b0ee8a022195c3aa45e94068ff2a4a0b4addff8
kessa99 <kipre325@mail.com>
2022-10-01 03:25:57 -0700

    print 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,18 +9,21 @@
 void more_numbers(void)
 {
 	int i;
-
-	for (i = 0 ; i <= 14 ; i++)
+	int j;
+	for (j = 0 ; j < 10 ; j++)
 	{
-		if (i >= 10)
+		for (i = 0 ; i <= 14 ; i++)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			else
+			{
+				_putchar(i + '0');
+			}
 		}
-		else
-		{
-			_putchar(i + '0');
-		}
-	}
 	_putchar('\n');
+	}
 }
