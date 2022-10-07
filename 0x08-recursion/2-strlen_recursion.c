f61a3a3f26bf391244e9e897407c6ec2b46ade56
kessa99 <kipre325@mail.com>
2022-10-06 21:20:40 -0700

    _strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns lenght
+ * @s : pointer
+ * Return: void
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0 ;
+	if (s == '\0')
+		return (len);
+	s++;
+	return (1 + _strlen_recursion(s));
+}
