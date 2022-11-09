f3f2b50a736a7f302c672d1c938fd3c48120ecb3
David <kessadavidkipre@gmail.com>
2022-11-09 05:05:52 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,35 +1,32 @@
 #include <stdio.h>
 /**
- * main - principal file
+ * main - principal
  *
  * Return: 0
  */
 int main(void)
 {
-	int i, c, d, u, n, q, q2;
-	char l[] = "789";
+	int i;
+	int j;
+	int k;
 
-	for (i = 0; i <= 788; i++)
+	for (i = 48; i < 56; i++)
 	{
-		n = i;
-		u = n % 10;
-		q = n / 10;
-		d = q % 10;
-		q2 = q / 10;
-		c = q2 % 10;
-		if (c < d && c < u && d < u)
+		for (j = i + 1; j < 57; j++)
 		{
-			putchar(c + '0');
-			putchar(d + '0');
-			putchar(u + '0');
-			putchar(',');
-			putchar(' ');
+			for (k = j + 1; k < 58; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != 55 || j != 56 || k != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
-	for (i = 0; i <= 2; i++)
-	{
-		putchar(l[i]);
-	}
-printf("\n");
+	putchar('\n');
 return (0);
 }
