2f881e90a5a73361c877c19d754c882dc09c26cf
Kessa99 <kessadavidkipre@gmail.com>
2022-11-16 04:10:10 -0800

    table_general

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,12 +10,10 @@ void print_times_table(int n)
 	int i, j, k;
 
 	if (n >= 15 || n <= 0)
-	{
 		return;
-	}
 	for (i = 0; i <= n; i++)
 	{
-		for(j = 0; j <= n ; j++)
+		for (j = 0; j <= n ; j++)
 		{
 			k = i * j;
 			if (k > 99)
@@ -37,11 +35,7 @@ void print_times_table(int n)
 				_putchar(k + '0');
 			}
 			else
-			{
-				_putchar(' ');
-				_putchar(' ');
 				_putchar(k + '0');
-			}
 			if (j != n)
 			{
 				_putchar(',');
