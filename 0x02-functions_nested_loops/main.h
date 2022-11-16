27dd3e49f78cc4cb9e226832018361a209ebfcb5
Kessa99 <kessadavidkipre@gmail.com>
2022-11-16 03:53:49 -0800

    table_general

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,7 +8,7 @@ int print_sign(int a);
 int _abs(int a);
 int print_last_digit(int a);
 void jack_bauer(void);
-void times_table(void);
+void print_times_table(int n);
 int add(int a, int b);
 void times_table(void);
 void print_to_98(int n);
