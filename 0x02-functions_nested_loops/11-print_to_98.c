1c74b9e62c6a5645e8bdefcf0a7d4f32f877aa46
Kessa99 <kessadavidkipre@gmail.com>
2022-11-22 07:52:04 -0800

    print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_to_98 - prin number from n to 98
  *
@@ -10,48 +10,27 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n > 0 && n != 98)
-	{
-		printf("%d, ", n);
-		while (n > 97)
-		{		
-			n = n - 1;
-           		printf("%d", n);
-            		printf(",");
-            		printf(" ");
-        	}
-		printf("98");
-	}
-	else if (n < 0)
-	{
-		printf("%d, ", n);
-		while (n < 0)
-		{
-			n = n + 1;
-			printf("%d", n);
-			printf(",");
-			printf(" ");
-		}	
-		for (i = 1 ; i < 97 ; i++)
-		{
-			printf("%d", i);
-			printf(",");
-			printf(" ");
-		}
-	printf("98");
-	}
-	else if (n == 98)
-	{
-		printf("98");
-	}
+	if (n > 98)
+	  {
+	    for (i = n; i > 98; i--)
+	      {
+		printf("%d ", i);
+		printf(",");
+	      }
+	    printf("98");
+	  }
+	else if (n < 98)
+	  {
+	    for (i = n; i < 98; i++)
+	      {
+		printf("%d ", i);
+		printf(",");
+	      }
+	    printf("98");
+	  }
 	else
-	{
-		for (i = 0 ; i < 97 ; i++)
-		{
-			printf("%d", i);
-			printf(",");
-			printf(" ");
-		}
-	printf("98");
-	}
+	  {
+	    printf("98");
+	  }
+	printf("\n");
 }
