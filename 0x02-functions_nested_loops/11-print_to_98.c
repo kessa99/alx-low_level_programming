ef0e725a8caf9f4471d0237f35bda9a85ae16a69
Kessa99 <kessadavidkipre@gmail.com>
2022-11-22 07:56:41 -0800

    print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 	    for (i = n; i > 98; i--)
 	      {
 		printf("%d", i);
-		printf(" ,");
+		printf(", ");
 	      }
 	    printf("98");
 	  }
@@ -24,7 +24,7 @@ void print_to_98(int n)
 	    for (i = n; i < 98; i++)
 	      {
 		printf("%d", i);
-		printf(" ,");
+		printf(", ");
 	      }
 	    printf("98");
 	  }
