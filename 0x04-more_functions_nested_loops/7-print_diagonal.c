9170b22739921a39b54e30bba97afdd557dc91cf
kessa99 <kessadavidkipre@gmail.com>
2022-12-19 01:31:58 +0000

    7-print_diagonal.c udapt

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,20 +9,18 @@
 void print_diagonal(int n)
 {
 
-	int i;
-	int j;
-
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
+	int i, j;
+	
 	for (i = 1 ; i <= n ; i++)
 	{
 		for (j = 1 ; j <= i ; j++)
 			_putchar(' ');
 		_putchar('\\');
-
-	_putchar('\n');
+		_putchar('\n');
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
 	}
 }
 
